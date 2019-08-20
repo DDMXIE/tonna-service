b945fbdd5da11493060ec3bdf1226644e69ae5e1
tony <793715596@qq.com>
2019-08-20 12:15:19 +0800

    TestUserController修复

--- src/main/java/com/tony/tonna/control/TestUserController.java
+++ src/main/java/com/tony/tonna/control/TestUserController.java
@@ -11,9 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
